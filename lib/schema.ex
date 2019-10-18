1b92821d59239804d5afe5eb3108eab195cbce23
woylie <git@woylie.de>
2019-10-18 18:51:24 +0200

    add filterable function to schema protocol

--- lib/schema.ex
+++ lib/schema.ex
@@ -5,20 +5,35 @@ defprotocol Flop.Schema do
 
   @fallback_to_any true
 
+  def filterable(struct)
   def sortable(struct)
 end
 
 defimpl Flop.Schema, for: Any do
   defmacro __deriving__(module, _struct, options) do
+    filterable_fields = Keyword.get(options, :filterable, [])
     sortable_fields = Keyword.get(options, :sortable, [])
 
     quote do
       defimpl Flop.Schema, for: unquote(module) do
+        def filterable(_) do
+          unquote(filterable_fields)
+        end
+
         def sortable(_) do
           unquote(sortable_fields)
         end
       end
     end
+  end
+
+  def filterable(struct) do
+    raise Protocol.UndefinedError,
+      protocol: @protocol,
+      value: struct,
+      description: """
+      Flop.Schema protocol must always be explicitly implemented.
+      """
   end
 
   def sortable(struct) do
