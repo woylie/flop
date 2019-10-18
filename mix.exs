c7a4743e915053b8a12be76b2773f88828b17703
woylie <git@woylie.de>
2019-10-18 17:13:05 +0200

    allow Elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Flop.MixProject do
     [
       app: :flop,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
