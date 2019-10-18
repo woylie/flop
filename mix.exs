1a780200bf46c992349d1c88b29e5f61ebcb2599
woylie <git@woylie.de>
2019-10-18 17:25:59 +0200

    configure dialyzer

--- mix.exs
+++ mix.exs
@@ -15,6 +15,9 @@ defmodule Flop.MixProject do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
     ]
   end
