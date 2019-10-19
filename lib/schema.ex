0efe4f97940ab76e98005a6710317a8522474bd6
woylie <git@woylie.de>
2019-10-19 03:07:22 +0200

    improve error message

--- lib/schema.ex
+++ lib/schema.ex
@@ -8,6 +8,22 @@ defprotocol Flop.Schema do
 end
 
 defimpl Flop.Schema, for: Any do
+  @instructions """
+  Flop.Schema protocol must always be explicitly implemented.
+
+  To do this, you have to derive Flop.Schema in your Ecto schema module.
+
+      @derive {Flop.Schema,
+               filterable: [:name, :species], sortable: [:name, :age, :species]}
+
+      schema "pets do
+        field :name, :string
+        field :age, :integer
+        field :species, :string
+        field :social_security_number, :string
+      end
+
+  """
   defmacro __deriving__(module, _struct, options) do
     filterable_fields = Keyword.get(options, :filterable, [])
     sortable_fields = Keyword.get(options, :sortable, [])
@@ -29,17 +45,13 @@ defimpl Flop.Schema, for: Any do
     raise Protocol.UndefinedError,
       protocol: @protocol,
       value: struct,
-      description: """
-      Flop.Schema protocol must always be explicitly implemented.
-      """
+      description: @instructions
   end
 
   def sortable(struct) do
     raise Protocol.UndefinedError,
       protocol: @protocol,
       value: struct,
-      description: """
-      Flop.Schema protocol must always be explicitly implemented.
-      """
+      description: @instructions
   end
 end
