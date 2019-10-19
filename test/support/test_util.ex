641929fcf2ab1f1c7525d62232a2f8e7fdd8c674
woylie <git@woylie.de>
2019-10-19 03:09:18 +0200

    add docstring

--- test/support/test_util.ex
+++ test/support/test_util.ex
@@ -1,6 +1,15 @@
 defmodule Flop.TestUtil do
   @moduledoc false
 
+  @doc """
+  A helper that transforms changeset errors into a map of messages.
+
+      assert {:error, changeset} = Accounts.create_user(%{password: "short"})
+      assert "password is too short" in errors_on(changeset).password
+      assert %{password: ["password is too short"]} = errors_on(changeset)
+
+  Brought to you by Phoenix.
+  """
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
