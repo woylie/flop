f94d9a396bbb588e089b7229dd662cb8fdc93b3c
Mathias Polligkeit <git@woylie.de>
2020-10-15 21:15:33 +0200

    add uncommitted new test files

---
+++ test/support/vegetable.ex
@@ -0,0 +1,18 @@
+defmodule Flop.Vegetable do
+  @moduledoc """
+  Defines an Ecto schema for testing.
+  """
+  use Ecto.Schema
+
+  @derive {Flop.Schema,
+           filterable: [:name, :family],
+           sortable: [:name],
+           default_limit: 50,
+           default_order_by: [:name],
+           default_order_directions: [:asc]}
+
+  schema "fruits" do
+    field :name, :string
+    field :family, :string
+  end
+end
