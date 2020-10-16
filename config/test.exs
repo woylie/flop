fd0925877c49406752a0fc5b05294021edc1f6dd
Mathias Polligkeit <git@woylie.de>
2020-10-16 12:43:32 +0200

    restrict stream data runs and run time locally

--- config/test.exs
+++ config/test.exs
@@ -11,4 +11,8 @@ config :flop, Flop.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :stream_data,
+  max_runs: if(System.get_env("CI"), do: 100, else: 50),
+  max_run_time: if(System.get_env("CI"), do: 3000, else: 50)
+
 config :logger, level: :warn
