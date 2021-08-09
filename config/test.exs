cbcdcb525c7e61a3fd7a3966f198c97e4003551c
Mathias Polligkeit <git@woylie.de>
2021-08-09 18:22:27 +0900

    increase max run time

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,6 @@ config :flop, Flop.Repo,
 
 config :stream_data,
   max_runs: if(System.get_env("CI"), do: 100, else: 50),
-  max_run_time: if(System.get_env("CI"), do: 3000, else: 50)
+  max_run_time: if(System.get_env("CI"), do: 3000, else: 200)
 
 config :logger, level: :warn
