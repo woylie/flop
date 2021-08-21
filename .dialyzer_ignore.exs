55cfb51f0af068dbfffd73461afc6294e5554bd1
Mathias Polligkeit <git@woylie.de>
2021-08-22 01:17:29 +0900

    update dialyzer ignore file

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,9 +1,11 @@
 [
-  ~r/lib\/flop\/schema\.ex.*no_return Function field_type\/2 has no local return\./,
-  ~r/lib\/flop\/schema\.ex.*no_return Function filterable\/1 has no local return\./,
-  ~r/lib\/flop\/schema\.ex.*no_return Function sortable\/1 has no local return\./,
+  ~r/lib\/flop\/schema\.ex.*no_return Function apply_order_by\/3 has no local return\./,
+  ~r/lib\/flop\/schema\.ex.*no_return Function cursor_dynamic\/3 has no local return\./,
   ~r/lib\/flop\/schema\.ex.*no_return Function default_limit\/1 has no local return\./,
   ~r/lib\/flop\/schema\.ex.*no_return Function default_order\/1 has no local return\./,
+  ~r/lib\/flop\/schema\.ex.*no_return Function field_type\/2 has no local return\./,
+  ~r/lib\/flop\/schema\.ex.*no_return Function filterable\/1 has no local return\./,
   ~r/lib\/flop\/schema\.ex.*no_return Function max_limit\/1 has no local return\./,
-  ~r/lib\/flop\/schema\.ex.*no_return Function pagination_types\/1 has no local return\./
+  ~r/lib\/flop\/schema\.ex.*no_return Function pagination_types\/1 has no local return\./,
+  ~r/lib\/flop\/schema\.ex.*no_return Function sortable\/1 has no local return\./
 ]
