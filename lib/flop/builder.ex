b8c377d9989f4ec2e7bd3797d491c2554a682452
Mathias Polligkeit <git@woylie.de>
2022-08-07 13:59:29 +0900

    refactor builder, add operators module

--- lib/flop/builder.ex
+++ lib/flop/builder.ex
@@ -2,12 +2,33 @@ defmodule Flop.Builder do
   @moduledoc false
 
   import Ecto.Query
-  import Flop.Misc
+  import Flop.Operators
 
   alias Flop.Filter
-  alias Flop.Misc
 
   require Logger
+
+  @operators [
+    :==,
+    :!=,
+    :empty,
+    :not_empty,
+    :>=,
+    :<=,
+    :>,
+    :<,
+    :in,
+    :contains,
+    :not_contains,
+    :like,
+    :=~,
+    :ilike,
+    :not_in,
+    :like_and,
+    :like_or,
+    :ilike_and,
+    :ilike_or
+  ]
 
   def filter(_, %Filter{field: nil}, c), do: c
   def filter(_, %Filter{value: nil}, c), do: c
@@ -24,64 +45,6 @@ defmodule Flop.Builder do
       filter
     )
   end
-
-  operator_opts = [
-    {:==, quote(do: field(r, ^var!(field)) == ^var!(value))},
-    {:!=, quote(do: field(r, ^var!(field)) != ^var!(value))},
-    {:empty, quote(do: is_nil(field(r, ^var!(field))) == ^var!(value))},
-    {:not_empty, quote(do: not is_nil(field(r, ^var!(field))) == ^var!(value))},
-    {:>=, quote(do: field(r, ^var!(field)) >= ^var!(value))},
-    {:<=, quote(do: field(r, ^var!(field)) <= ^var!(value))},
-    {:>, quote(do: field(r, ^var!(field)) > ^var!(value))},
-    {:<, quote(do: field(r, ^var!(field)) < ^var!(value))},
-    {:in, quote(do: field(r, ^var!(field)) in ^var!(value))},
-    {:contains, quote(do: ^var!(value) in field(r, ^var!(field)))},
-    {:not_contains, quote(do: ^var!(value) not in field(r, ^var!(field)))},
-    {:like, quote(do: like(field(r, ^var!(field)), ^var!(value))),
-     :add_wildcard},
-    {:=~, quote(do: ilike(field(r, ^var!(field)), ^var!(value))),
-     :add_wildcard},
-    {:ilike, quote(do: ilike(field(r, ^var!(field)), ^var!(value))),
-     :add_wildcard},
-    {:not_in, quote(do: ^var!(d)), nil,
-     """
-     d =
-       if nil in value do
-        filtered_value = Enum.filter(value, &(not is_nil(&1)))
-        dynamic(<<<binding>>>, field(r, ^field) not in ^filtered_value and not is_nil(field(r, ^field)))
-       else
-        dynamic(<<<binding>>>, field(r, ^field) not in ^value)
-       end
-     """},
-    {:like_and, quote(do: ^var!(d)), :split_search_text,
-     """
-     d =
-       Enum.reduce(value, true, fn value, dynamic ->
-         dynamic(<<<binding>>>, ^dynamic and like(field(r, ^field), ^value))
-       end)
-     """},
-    {:like_or, quote(do: ^var!(d)), :split_search_text,
-     """
-     d =
-       Enum.reduce(value, false, fn value, dynamic ->
-         dynamic(<<<binding>>>, ^dynamic or like(field(r, ^field), ^value))
-       end)
-     """},
-    {:ilike_and, quote(do: ^var!(d)), :split_search_text,
-     """
-     d =
-       Enum.reduce(value, true, fn value, dynamic ->
-         dynamic(<<<binding>>>, ^dynamic and ilike(field(r, ^field), ^value))
-       end)
-     """},
-    {:ilike_or, quote(do: ^var!(d)), :split_search_text,
-     """
-     d =
-       Enum.reduce(value, false, fn value, dynamic ->
-         dynamic(<<<binding>>>, ^dynamic or ilike(field(r, ^field), ^value))
-       end)
-     """}
-  ]
 
   defp build_op(c, schema_struct, {:compound, fields}, %Filter{op: op} = filter)
        when op in [
@@ -154,37 +117,15 @@ defmodule Flop.Builder do
     c
   end
 
-  for operator_and_condition <- operator_opts do
-    {op, condition, preprocessor, dynamic_builder} =
-      case operator_and_condition do
-        {operator, condition} -> {operator, condition, nil, nil}
-        {operator, condition, func} -> {operator, condition, func, nil}
-        {operator, condition, func, df} -> {operator, condition, func, df}
-      end
-
-    preprocessing =
-      unless is_nil(preprocessor) do
-        quote do
-          var!(value) = Misc.unquote(preprocessor)(var!(value))
-        end
-      end
+  for op <- @operators do
+    {fragment, prelude, combinator} = op_config(op)
 
     defp build_op(c, _schema_struct, {:normal, field}, %Filter{
            op: unquote(op),
            value: value
          }) do
-      unquote(preprocessing)
-
-      # prevent unused variable warning for operators that don't use value
-      _ = value
-
-      unquote(quote_dynamic(dynamic_builder, "[r]"))
-
-      unquote(
-        quote do
-          dynamic([r], ^var!(c) and unquote(condition))
-        end
-      )
+      unquote(prelude)
+      build_dynamic(unquote(fragment), false, unquote(combinator))
     end
 
     defp build_op(
@@ -196,18 +137,8 @@ defmodule Flop.Builder do
              value: value
            }
          ) do
-      unquote(preprocessing)
-
-      # prevent unused variable warning for operators that don't use value
-      _ = value
-
-      unquote(quote_dynamic(dynamic_builder, "[{^binding, r}]"))
-
-      unquote(
-        quote do
-          dynamic([{^var!(binding), r}], ^var!(c) and unquote(condition))
-        end
-      )
+      unquote(prelude)
+      build_dynamic(unquote(fragment), true, unquote(combinator))
     end
   end
 
