65d7edb752159b700b8384cf354c958361abbeda
Mathias Polligkeit <git@woylie.de>
2023-06-27 20:35:53 +0900

    update logger config

--- config/test.exs
+++ config/test.exs
@@ -15,4 +15,4 @@ config :stream_data,
   max_runs: if(System.get_env("CI"), do: 100, else: 50),
   max_run_time: if(System.get_env("CI"), do: 3000, else: 200)
 
-config :logger, level: :warn
+config :logger, level: :warning
