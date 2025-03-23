5624288db118f13656e5ab11ad41b93ee5978352
Matt Lewis <domattthings@gmail.com>
2025-03-23 13:13:42 -0400

    Add sorting by custom fields
    Initial implementation of custom field sorting. This allows users to
    define fields they want to do arbitrary sorting without having to
    alias a field.
    
    Another bonus is you can technically pass in variables to do some
    pretty fun dynamic sorting. Right now I'm using them to surface
    relavent information to a user (PostGIS coordinate compared to user
    location).
    
    For now we handle those custom variables in our code and pass them
    in. In the future it would be cool to see that a bit more formalized
    since we have to basically repeat some boilerplate for verifying some
    of these custom sort variables.
    
    One additional sticky situation I found while doing this
    implementation is cursor fields, given other complex fields are not
    supported I opted to do the same here. However in theory it wouldn't
    be much work to support, it really just requires that a `get_field/1`
    callback can execute at runtime (a bit weird though if you rely on
    runtime options like I'm doing above), then we would just need to
    validate that a filter function is set in addition to the sorter
    function for the custom field.

--- lib/flop/schema.ex
+++ lib/flop/schema.ex
@@ -677,6 +677,7 @@ defprotocol Flop.Schema do
         extra: %{
           type: :custom,
           filter: {MyApp.Pet, :reverse_name_filter, []},
+          sorter: nil,
           bindings: []
         }
       }
