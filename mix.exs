05860aeaf4c1cb400f116d9ec8c66aaa16890cb1
Patric Vinicios <psdev7@gmail.com>
2026-07-20 12:55:01 -0300

    chore(deps): bump postgrex to 0.22.3 to fix hex.audit advisory

--- mix.exs
+++ mix.exs
@@ -77,7 +77,7 @@ defmodule Flop.MixProject do
       {:excoveralls, "0.18.5", only: :test},
       {:myxql, "0.9.0", only: :test},
       {:nimble_options, "~> 1.0"},
-      {:postgrex, "0.22.2", only: :test},
+      {:postgrex, "0.22.3", only: :test},
       {:ecto_sqlite3, "== 0.24.1", only: :test},
       {:stream_data, "1.3.0", only: [:dev, :test]}
     ]
