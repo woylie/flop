6cf11703635e5d0bc29ff18907aec1a7f96c0c5a
woylie <13847569+woylie@users.noreply.github.com>
2026-08-07 15:23:21 +0200

    raise error when pattern operator is used with non-stringv value

--- test/base/flop/misc_test.exs
+++ test/base/flop/misc_test.exs
@@ -1,5 +1,23 @@
 defmodule Flop.MiscTest do
   use ExUnit.Case, async: true
 
+  alias Flop.Misc
+
   doctest Flop.Misc, import: true
+
+  describe "wildcard helpers" do
+    test "raise if called with a non-string value" do
+      for value <- [["George"], %{"a" => "George"}, 8, nil] do
+        for fun <- [
+              &Misc.add_wildcard/1,
+              &Misc.add_wildcard_suffix/1,
+              &Misc.add_wildcard_prefix/1
+            ] do
+          assert_raise ArgumentError,
+                       ~r/invalid filter value for pattern operator/,
+                       fn -> fun.(value) end
+        end
+      end
+    end
+  end
 end
